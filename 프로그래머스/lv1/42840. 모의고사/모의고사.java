6bc1944cf470f198212af7287f40a73f3205e7ac
argo2954 <109727039+argo2954@users.noreply.github.com>
2022-07-31 16:18:21 +0900

    [level 1] Title: 모의고사, Time: 3.98 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.java
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -6,21 +6,18 @@ class Solution {
         int[] arr3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}; //3번 수포자 루틴
 
         int alen = answers.length; //answers의 length 
-        int[] tmp = new int[3];
+        int[] score = new int[3];
         for(int i=0; i<alen; i++){
-            if(arr1[i%5] == answers[i]) tmp[0]++;
-            if(arr2[i%8] == answers[i]) tmp[1]++;
-            if(arr3[i%10] == answers[i]) tmp[2]++;
+            if(arr1[i%5] == answers[i]) score[0]++;
+            if(arr2[i%8] == answers[i]) score[1]++;
+            if(arr3[i%10] == answers[i]) score[2]++;
         }
       
-        int max = Arrays.stream(tmp).max().getAsInt();//tmp배열에서 최대값 찾기
-        int size = 0;
-        for(int i : tmp) if(i==max) size++;
-        
-        int[] answer = new int[size];
-        for(int i=0,idx=0; i<3; i++)
-            if(tmp[i]==max)
-                answer[idx++] = i+1;
-        return answer;
+        int max = Arrays.stream(score).max().getAsInt();//tmp배열에서 최대값 찾기
+        ArrayList<Integer> list = new ArrayList<>(); //Alist생성
+        for(int i=0; i<3; i++)
+            if(score[i]==max)
+                list.add(i+1);
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
