05b10600ce078c9675e66cf0d46dc536447dd7d8
argo2954 <109727039+argo2954@users.noreply.github.com>
2022-08-05 16:39:24 +0900

    [Silver III] Title: N과 M (4), Time: 220 ms, Memory: 17968 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,27 @@
+import java.io.*;
+class Main {
+  public static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+  public static BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+  public static int n, m;
+  
+  public static void main(String[] args) throws IOException{
+    String[] s = in.readLine().split(" ");
+    n = Integer.parseInt(s[0]);
+    m = Integer.parseInt(s[1]);
+    
+    f(0,1,"");
+
+    in.close();
+    out.close();
+  }
+  static void f(int d, int idx, String s) throws IOException{
+    if(d==m){
+      out.write(s + "\n");
+      return;
+    }
+    for(int i=idx; i<=n; i++){
+      f(d+1, i, s+i+" ");
+    }
+    
+  }
+}
