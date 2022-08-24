dabe56623e1678d3e070d8a9979bbc6eff7e9ab7
argo2954 <109727039+argo2954@users.noreply.github.com>
2022-08-24 15:26:55 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 140 ms, Memory: 15848 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,30 +1,32 @@
-import java.util.*;
+import java.io.*;
 class Main {
-  public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
-    int N = in.nextInt();
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    int n = Integer.parseInt(br.readLine());
+    int answer = n;    
+    while(n-->0){
+      String s= br.readLine();
+      int[] count = new int[26];
+      for(int i=0; i<26; i++) count[i] = 100;
+      int len = s.length();
 
-    int answer = N;
-    while(N-->0){
-      boolean[] cmp = new boolean[26]; //기본 false, 알파벳이 나오면 true
-      String s = in.next();
-      cmp[s.charAt(0)-'a'] = true; //s의 첫 번째 알파벳에 해당하는 index = true
-
-      boolean check = true;
-      for(int i=1; i<s.length(); i++){//s 순회
-        //해당되는 알파벳이 이 전에 나왔고, 직전 알파벳과 해당 알파벳이 다를경우
-        //check = false 및 순회종료
-        if(cmp[s.charAt(i)-'a'] & s.charAt(i) != s.charAt(i-1)){
-          check = false; break;
+      for(int i=0; i<len; i++){
+        char c = s.charAt(i);
+        if(count[c-'a']!=100){
+          if(count[c-'a']!=i-1){
+            answer--;
+            break;
+          }
         }
-        //해당되는 알파벳이 이 전에 나오지 않았다면
-        //알파벳에 해당되는 cmp = true
-        else
-          cmp[s.charAt(i)-'a'] = true;
+        count[c-'a'] = i;
       }
-      if(!check) answer--;
     }
-
-    System.out.println(answer);
+    
+    bw.write(answer + "\n");
+    br.close();
+    bw.close();
   }
+
+ 
 }
