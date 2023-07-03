ef788bfa36cc2d2292a9a50cab20d799b7974458
wnso <109727039+wnso-kim@users.noreply.github.com>
2023-07-03 13:22:25 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 1068 ms, Memory: 303100 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,75 +1,98 @@
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+
 class Main {
-  public static void main(String[] args) throws IOException{
-    init();
-    f(0,num[0],new boolean[n-1]);
-    
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    //bw.write(ss + "\n");
-    bw.write(max+"\n"); bw.write(min+"\n");
-    bw.close();
- }
-  //전역 변수
-  static int n;
-  static int[] num; //피연산자
-  static int[] op;  //연산자 -> +,-,x,/ 순
-  static int max = -100000000;
-  static int min = +100000000;
-  //static String ss = "";
-  //초기화 함수
-  //입력 및 배열 저장
-  public static void init() throws IOException{
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    //첫 째줄 입력
-    n = Integer.parseInt(br.readLine());
-    num = new int[n];
-    op = new int[n-1];
-    //둘 째줄 입력
-    String[] s1 = br.readLine().split(" ");
-    for(int i=0; i<n; i++) num[i] = Integer.parseInt(s1[i]);
-    //셋 째줄 입력
-    String[] s2 = br.readLine().split(" ");
-    for(int i=0,j=0; i<4; i++){
-      int t = Integer.parseInt(s2[i]) + j;
-      while(j<t){
-        op[j] = i+1;
-        j++;
-      }
+    public static void main(String[] args) throws Exception {
+        Operator operator = new Operator();
+        operator.input();
+        operator.changeO();
+        operator.start();
+        operator.print();
     }
-    br.close();
-  }
-  //연산 함수
-  public static int opr(int sum, int num, int op){
-    switch(op){
-      case 1:
-        return sum + num;
-      case 2:
-        return sum - num;
-      case 3:
-        return sum * num;
-      case 4:
-        return sum / num;
+     
+    static class Operator{
+        int N;      // 수의 개수
+        int[] A;    // 수들
+        int[] O;    // 연산자들의 개수(+,-,*,/)
+        boolean[] bool; // 브루트포스를 위한 boolean
+        String S = ""; // 문자열로 연결된 연산자들
+        int min = 1000000000;
+        int max = -1000000000;
+        // 기본 생성자 사용
+
+        // 입력 함수
+        void input() throws IOException{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            N = Integer.parseInt(br.readLine());
+
+            A = new int[N];
+            O = new int[4];
+            bool = new boolean[N];
+
+            String[] inputA = br.readLine().split(" ");
+            String[] inputO = br.readLine().split(" ");;
+
+            for(int i=0; i<N; i++) A[i] = Integer.parseInt(inputA[i]);
+            for(int i=0; i<4; i++) O[i] = Integer.parseInt(inputO[i]);
+        }
+
+        // 연산자 전환: [+ - * / ] -> [+++--*/]
+        void changeO(){
+            for(int i=0; i<4; i++){
+                S += (""+i).repeat(O[i]);
+            }
+        }
+
+        // 연산 -> 2개의 숫자
+        int Operate(int a, int b, String o){
+            switch(o){
+                case "0":
+                    return a+b;
+                case "1":
+                    return a-b;
+                case "2":
+                    return a*b;
+                case "3":
+                    return a/b;
+                default:
+                    return 0;
+            }
+        }
+
+        // 전체 연산
+        void start(){
+            find(A[0], 1);
+        }
+        void find(int result, int index){    //index: 
+            if(index==N){
+                if(result>max) max = result;
+                if(result<min) min = result;
+                return;
+            }
+            // 이전 결과와 연산 후 다음 숫자로 넘김
+            for(int i=0; i<N-1; i++){
+                if(bool[i]) 
+                    continue;
+                
+                int tmp = Operate(result, A[index], S.substring(i, i+1));
+                bool[i] = true;
+                find(tmp, index+1);
+                bool[i] = false;
+            }
+        }
+
+        // 결과 출력
+        void print() throws IOException{
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+            bw.write(max + "\n" + min + "\n");
+            bw.flush();
+            bw.close();
+        }
     }
-    return sum;
-  }
-  //백트레킹 함수
-  //depth = 현재 깊이, n-1과 같아지면 함수 끝
-  public static void f(int depth, int sum, boolean[] b){
-    //최종 깊이에 도달한 경우 max와 min을 조정하고 함수 빠져나옴
-    if(depth==n-1){
-      if(sum>max) max = sum;
-      if(sum<min) min = sum;
-      return;
-    }
-    //연산
-    for(int i=0; i<n-1; i++){
-      if(b[i]) continue;
-      else{
-        b[i] = true;
-        //ss += sum + " " +op[depth] + " " + num[depth+1] + " = " + opr(sum, num[depth+1], op[depth]) + "\n";
-        f(depth+1, opr(sum, num[depth+1], op[i]), b);
-        b[i] = false;
-      }
-    } 
-  }
 }
