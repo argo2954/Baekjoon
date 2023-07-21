2994cefb6ac2fd8546ea07fd7c1fe35ca74bf36e
wnso <109727039+wnso-kim@users.noreply.github.com>
2023-07-21 13:22:15 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 892 ms, Memory: 103944 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -2,17 +2,22 @@ import java.io.*;
 import java.util.*;
 class Main {
   public static void main(String[] args) throws IOException{
-    //입력
   BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
   BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
     int N = Integer.parseInt(in.readLine());
-    PriorityQueue<Integer> heap = new PriorityQueue<Integer>();
-    for(int i=0; i<N; i++)
-      heap.add(Integer.parseInt(in.readLine()));
-
+    boolean[] barr = new boolean[2000002];
+    
+    //입력
+    for(int i=0; i<N; i++){
+      int input = Integer.parseInt(in.readLine());
+      barr[input+1000001] = true;
+    }
+      
     //정렬 및 출력
-    for(int i=0; i<N; i++)
-      out.write(heap.poll() + "\n");
+    for(int i=1; i<2000002; i++){
+      if(barr[i]==true)
+        out.write((i-1000001)+"\n");
+    }
     
     in.close();
     out.close();
