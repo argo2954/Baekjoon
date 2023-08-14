5ad693e23326d648d8fb8862e995dd7282a9de54
wnso <109727039+wnso-kim@users.noreply.github.com>
2023-08-14 19:07:47 +0900

    [Silver I] Title: 회의실 배정, Time: 512 ms, Memory: 44428 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // input & init | N & schedules
+        int N = Integer.parseInt(br.readLine());
+        
+        int[][] schedules = new int[N][2];
+        
+        for(int i=0; i<N; i++){
+            StringTokenizer stk = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(stk.nextToken());
+            int end = Integer.parseInt(stk.nextToken());
+            schedules[i][0] = start;
+            schedules[i][1] = end;
+        }
+
+        // sort | schedules -> 1.끝나는 시간(오름차순) 2.시작시간(오름차순)
+        Arrays.sort(schedules, (arr1, arr2)->{
+            return arr1[1]!=arr2[1]? arr1[1]-arr2[1]: arr1[0]-arr2[0];
+        });
+        
+        // solve | 
+        int answer = 0;
+        int end = 0;
+        for(int[] schedule: schedules){
+            if(end <= schedule[0]){
+                answer++;
+                end = schedule[1];
+            }
+        }
+        
+        System.out.println(answer);
+    }
+
+}
