7668c26bedcd78733105f8894d35300a644ab35b
wnso <109727039+wnso-kim@users.noreply.github.com>
2023-12-02 23:46:50 +0900

    [level 2] Title: 피보나치 수, Time: 3.20 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        int[] fibonacci = new int[n+1];
+        fibonacci[0] = 0;
+        fibonacci[1] = 1;
+        
+        for(int i=2; i<=n; i++){
+            fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
+            fibonacci[i] = fibonacci[i] % 1234567;
+        }
+        
+        return fibonacci[n];
+    }
+}
