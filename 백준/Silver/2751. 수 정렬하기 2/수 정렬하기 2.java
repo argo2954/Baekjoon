6e25e334600b9cb48b716ea05d68a4c0a1390558
wnso <109727039+wnso-kim@users.noreply.github.com>
2024-02-12 13:51:50 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 768 ms, Memory: 175820 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,25 +1,62 @@
-import java.io.*;
-import java.util.*;
-class Main {
-  public static void main(String[] args) throws IOException{
-  BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-  BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
-    int N = Integer.parseInt(in.readLine());
-    boolean[] barr = new boolean[2000002];
-    
-    //입력
-    for(int i=0; i<N; i++){
-      int input = Integer.parseInt(in.readLine());
-      barr[input+1000001] = true;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static void mergeSort(int[] arr){
+        int[] tmp = new int[arr.length];
+        mergeSort(arr, tmp, 0, arr.length-1);
     }
-      
-    //정렬 및 출력
-    for(int i=1; i<2000002; i++){
-      if(barr[i]==true)
-        out.write((i-1000001)+"\n");
+
+    static void mergeSort(int[] arr, int[] tmp, int start, int end){
+        if(start>=end) return;
+
+        int mid = (start+end)/2;
+        mergeSort(arr, tmp, start, mid);
+        mergeSort(arr, tmp, mid+1, end);
+        merge(arr, tmp, start, mid, end);
     }
-    
-    in.close();
-    out.close();
-  }
+
+    static void merge(int[] arr, int[] tmp, int start, int mid, int end){
+        // 임시 배열에 값 저장|
+        for(int i=start; i<=end; i++){
+            tmp[i] = arr[i];
+        }
+
+        int part1 = start;
+        int part2 = mid+1;
+        int index = start;
+        while(part1<=mid && part2<=end){
+            if(tmp[part1] <= tmp[part2])
+                arr[index++] = tmp[part1++];
+            else
+                arr[index++] = tmp[part2++];
+        }
+
+        for(;part1<=mid;){
+            arr[index++] = tmp[part1++];
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // input| N
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        // input| array
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        // sort| merge-sort
+        mergeSort(arr);
+
+        // print
+        StringBuilder result = new StringBuilder();
+        for(int num: arr)
+            result.append(num+"\n");
+
+        System.out.print(result);
+        br.close();
+    }
 }
