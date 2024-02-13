e3fc217d29ae75d97e063729624d5875cb91a032
wnso <109727039+wnso-kim@users.noreply.github.com>
2024-02-13 21:51:16 +0900

    [Silver IV] Title: 설탕 배달, Time: 80 ms, Memory: 11500 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,22 +1,24 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
 
-        int answer = -1;
-        for(int i=N/5; i>=0; i--){
-            int mod = N - 5*i;
 
-            if(mod%3 == 0){
-                answer = i+(mod/3);
-                break;
-            }
-        }
-
-        System.out.println(answer);
-        br.close();
-    }
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		int five = N/5;
+		int third = N%5/3;
+		
+		while((five*5 + third*3)!=N && five>=0) {
+			five--;
+			third = (N-(five*5))/3;
+		}
+		
+		int result = five>=0? five+third: -1;
+		System.out.println(result);
+	}
 }
