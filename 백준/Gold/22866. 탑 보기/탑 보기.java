7b9afe9f7a8b5ec30429d99ab2ca1f1aa87a9bd6
wnso <109727039+wnso-kim@users.noreply.github.com>
2024-06-30 19:45:20 +0900

    [Gold III] Title: 탑 보기, Time: 352 ms, Memory: 42824 KB -BaekjoonHub

---
+++ 백준/Gold/22866. 탑 보기/탑 보기.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N;
+    static int[] buildings;
+    static int[][] dp;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        N = Integer.parseInt(br.readLine()); 
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        buildings = new int[N];
+        for(int i=0; i<N; i++)  buildings[i] = Integer.parseInt(stk.nextToken());
+        
+        dp = new int[N][3];
+
+        int max = buildings[0];
+        for(int i=1; i<N; i++){
+            if(max < buildings[i]){
+                max = buildings[i];
+                continue;
+            }
+
+            for(int j=i-1; j>=0; j--){
+                if(buildings[j] > buildings[i]){
+                    dp[i][0] = dp[j][0]+1;
+                    dp[i][2] = j+1;
+                    break;
+                }
+            }
+        }
+
+        max = buildings[N-1];
+        for(int i=N-2; i>=0; i--){
+            if(buildings[i] > max){
+                max = buildings[i];
+                continue;
+            }
+
+            for(int j=i+1; j<N; j++){
+                if(buildings[i] < buildings[j]){
+                    dp[i][1] = dp[j][1]+1;
+
+                    if(dp[i][0] == 0 || (i - dp[i][2]+1) > (j-i)){
+                        dp[i][2] = j+1;
+                    }
+
+                    break;
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<N; i++){
+            int sum = dp[i][0] + dp[i][1];
+
+            sb.append(sum).append(" ");
+            if(sum != 0) sb.append(dp[i][2]);
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
